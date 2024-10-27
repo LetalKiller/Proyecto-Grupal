7cbe1ee2c507cac15e7dcd6436516dd9903fa836
Daniel Molina <danielmolina3003@gmail.com>
2024-10-27 15:27:44 -0600

    cambios-moverse-entre-pantallas

--- app/src/main/java/com/example/proyecto_grupal/Uniforme.kt
+++ app/src/main/java/com/example/proyecto_grupal/Uniforme.kt
@@ -1,6 +1,8 @@
 package com.example.proyecto_grupal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,25 @@ class Uniforme : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnP: Button = findViewById(R.id.botonP);
+        btnP.setOnClickListener ({
+            val intent1: Intent = Intent(this, Plantilla::class.java)
+            startActivity(intent1)
+        })
+        val btnH: Button = findViewById(R.id.botonH);
+        btnH.setOnClickListener ({
+            val intent2: Intent = Intent(this, Historia::class.java)
+            startActivity(intent2)
+        })
+        val btnU: Button = findViewById(R.id.botonU);
+        btnU.setOnClickListener ({
+            val intent3: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent3)
+        })
+        val btnE: Button = findViewById(R.id.botonE);
+        btnE.setOnClickListener ({
+            val intent4: Intent = Intent(this, Estadio::class.java)
+            startActivity(intent4)
+        })
     }
 }
