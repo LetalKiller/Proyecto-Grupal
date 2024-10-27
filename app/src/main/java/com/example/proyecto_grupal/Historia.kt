b571fb853135bcb77f74980cb3873d336482c62a
Daniel Molina <danielmolina3003@gmail.com>
2024-10-27 15:16:50 -0600

    cambiosCreacionPantallas

---
+++ app/src/main/java/com/example/proyecto_grupal/Historia.kt
@@ -0,0 +1,20 @@
+package com.example.proyecto_grupal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Historia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_historia)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
